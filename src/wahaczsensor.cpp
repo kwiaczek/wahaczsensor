2638124cfaa99e9456d4bfe3806a897414e2cd46
Kacper Wiączek <kacper.wiaczek00@gmail.com>
2024-06-13 22:43:23 +0200

    ff

--- src/wahaczsensor.cpp
+++ src/wahaczsensor.cpp
@@ -2,16 +2,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <vl53l5cx_api.h>
+#include <cstring>
 
 #include "Sensorvl53l5cx.hpp"
 #include "hardware/i2c.h"
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
+#include "pico/i2c_slave.h"
 
 #define I2C_SDA 0
 #define I2C_SCL 1
 #define PIN_LPn 2
 #define PIN_LPn2 3
+#define SI2C_SDA 14
+#define SI2C_SCL 15
 
 std::shared_ptr<Sensorvl53l5cx> leftSensor;
 std::shared_ptr<Sensorvl53l5cx> rightSensor;
@@ -21,6 +25,16 @@ void poll() {
     leftSensor->pollForData();
     rightSensor->pollForData();
     sleep_ms(1000);
+  }
+}
+
+uint8_t tx[4];
+
+static void i2cHandler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+  if (event == I2C_SLAVE_REQUEST)
+  {
+    printf("REQUEST FULLFIELED! CALLBACK FROM HELL!\n %d %d \n", (I2C_SLAVE_REQUEST == event), event);
+	  i2c_write_raw_blocking(i2c, tx, 4);
   }
 }
 
@@ -35,25 +49,49 @@ int main(void) {
   gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
   gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
 
+
+	i2c_init(i2c1, 100000);
+  gpio_set_function(SI2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(SI2C_SCL, GPIO_FUNC_I2C);
+	i2c_slave_init(i2c1, 0x21, &i2cHandler);
+
+
   sleep_ms(3000);
   puts("SwingArm Sensors have started....\n");
 
   leftSensor = std::make_shared<Sensorvl53l5cx>(vl53l5cx_i2c, PIN_LPn);
   rightSensor = std::make_shared<Sensorvl53l5cx>(vl53l5cx_i2c, PIN_LPn2);
 
-  leftSensor->setAddress(0x16);
+  //leftSensor->setAddress(0x16);
 
-  if (!(leftSensor->initialize() && rightSensor->initialize())) {
-    return -1;
-  }
+
+  leftSensor->initialize();
+  rightSensor->initialize();
+  //if (!(leftSensor->initialize() && rightSensor->initialize())) {
+  //  return -1;
+  //}
 
   printf("successfully initialized!");
 
   multicore_launch_core1(poll);
 
   while (true) {
+    /*
     printf("%d --- %d\n", leftSensor->getLatestDistance(),
            rightSensor->getLatestDistance());
-    sleep_ms(500);
+    */
+    
+    auto x = leftSensor->getLatestDistance();
+    auto y = rightSensor->getLatestDistance();
+
+    tx[0] = 1;
+    tx[1] = 2;
+    tx[2] = 3;
+    tx[3] = 4;
+
+
+    //std::memcpy(tx, &x, 2);
+    //std::memcpy(tx+2, &y, 2);
+
   }
 }
