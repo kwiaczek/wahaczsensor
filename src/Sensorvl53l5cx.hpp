9a862b21cc9e3170a37ad06bcd2cdc69db08a66b
Kacper Wiączek <kacper.wiaczek00@gmail.com>
2024-06-29 10:25:32 +0200

    refactor

--- src/Sensorvl53l5cx.hpp
+++ src/Sensorvl53l5cx.hpp
@@ -4,21 +4,22 @@
 #include "vl53l5cx_api.h"
 #include <memory>
 
-class Sensorvl53l5cx {
-  std::shared_ptr<i2c_inst_t> i2c_instance;
-  uint8_t lpn_pin;
-  int16_t latestDistance; // TODO: maybe take into account more than 1 reading
-  VL53L5CX_Configuration config;
+class Sensorvl53l5cx
+{
+    std::shared_ptr<i2c_inst_t> i2c_instance;
+    uint8_t lpn_pin;
+    int16_t latestDistance; // TODO: maybe take into account more than 1 reading
+    VL53L5CX_Configuration config;
 
-public:
-  Sensorvl53l5cx(std::shared_ptr<i2c_inst_t> i2c_instance, uint8_t lpn_pin);
-  ~Sensorvl53l5cx();
+  public:
+    Sensorvl53l5cx(std::shared_ptr<i2c_inst_t> i2c_instance, uint8_t lpn_pin);
+    ~Sensorvl53l5cx();
 
-  bool initialize();
-  bool setAddress(uint8_t address);
-  void pollForData();
-  int16_t getLatestDistance();
+    bool initialize();
+    bool setAddress(uint8_t address);
+    void pollForData();
+    int16_t getLatestDistance();
 
-private:
-  void setLpnPinState(uint8_t state);
+  private:
+    void setLpnPinState(uint8_t state);
 };
--- src/wahaczsensor.cpp
+++ src/wahaczsensor.cpp
@@ -1,14 +1,14 @@
+#include <cstring>
 #include <platform.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <vl53l5cx_api.h>
-#include <cstring>
 
 #include "Sensorvl53l5cx.hpp"
 #include "hardware/i2c.h"
+#include "pico/i2c_slave.h"
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
-#include "pico/i2c_slave.h"
 
 #define I2C_SDA 0
 #define I2C_SCL 1
@@ -20,100 +20,105 @@
 std::shared_ptr<Sensorvl53l5cx> leftSensor;
 std::shared_ptr<Sensorvl53l5cx> rightSensor;
 
-void poll() {
-  while (true) {
-    leftSensor->pollForData();
-    rightSensor->pollForData();
-    sleep_ms((slave_i2c_data.memory[5] > 0) ? slave_i2c_data.memory[4] : 100);
-  }
+void poll()
+{
+    while (true)
+    {
+        leftSensor->pollForData();
+        rightSensor->pollForData();
+        sleep_ms((slave_i2c_data.memory[5] > 0) ? slave_i2c_data.memory[4] : 100);
+    }
 }
 
-static struct{
+static struct
+{
     uint8_t address;
     uint8_t memory[0xFF + 1];
     bool address_set;
 } slave_i2c_data;
 
-static void i2cHandler(i2c_inst_t *i2c, i2c_slave_event_t event) {
-  /*if (event == I2C_SLAVE_REQUEST)
-  {
-    printf("REQUEST FULLFIELED! CALLBACK FROM HELL!\n %d %d \n", (I2C_SLAVE_REQUEST == event), event);
-	  i2c_write_raw_blocking(i2c, tx, 4);
-  }*/
-  switch (event)
-  {
-  case I2C_SLAVE_RECEIVE:
-    if (!slave_i2c_data.address_set) {
-        slave_i2c_data.address = i2c_read_byte_raw(i2c);
-        slave_i2c_data.address_set = true;
-      }
-    else {
-        slave_i2c_data.memory[slave_i2c_data.address] = i2c_read_byte_raw(i2c);
-      }
-    break;
+static void i2cHandler(i2c_inst_t *i2c, i2c_slave_event_t event)
+{
+    /*if (event == I2C_SLAVE_REQUEST)
+    {
+      printf("REQUEST FULLFIELED! CALLBACK FROM HELL!\n %d %d \n", (I2C_SLAVE_REQUEST == event), event);
+        i2c_write_raw_blocking(i2c, tx, 4);
+    }*/
+    switch (event)
+    {
+    case I2C_SLAVE_RECEIVE:
+        if (!slave_i2c_data.address_set)
+        {
+            slave_i2c_data.address = i2c_read_byte_raw(i2c);
+            slave_i2c_data.address_set = true;
+        }
+        else
+        {
+            slave_i2c_data.memory[slave_i2c_data.address] = i2c_read_byte_raw(i2c);
+        }
+        break;
         i2c_write_byte_raw(i2c, slave_i2c_data.memory[slave_i2c_data.address++]);
-  case I2C_SLAVE_REQUEST: 
-    break;
-  case I2C_SLAVE_FINISH:
+    case I2C_SLAVE_REQUEST:
+        break;
+    case I2C_SLAVE_FINISH:
         slave_i2c_data.address_set = false;
         slave_i2c_data.address = 0;
         break;
-  default:
-    printf("Unrecognised request: %d\n", event);
-    break;
-  }
+    default:
+        printf("Unrecognised request: %d\n", event);
+        break;
+    }
 }
 
-int main(void) {
-  // pico settings
-  stdio_init_all();
-  std::shared_ptr<i2c_inst_t> vl53l5cx_i2c = std::make_shared<i2c_inst_t>();
-  vl53l5cx_i2c->hw = i2c0_hw;
-  vl53l5cx_i2c->restart_on_next = false;
+int main(void)
+{
+    // pico settings
+    stdio_init_all();
+    std::shared_ptr<i2c_inst_t> vl53l5cx_i2c = std::make_shared<i2c_inst_t>();
+    vl53l5cx_i2c->hw = i2c0_hw;
+    vl53l5cx_i2c->restart_on_next = false;
 
-  i2c_init(vl53l5cx_i2c.get(), 400 * 1000); // 400kHz
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    i2c_init(vl53l5cx_i2c.get(), 400 * 1000); // 400kHz
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
 
+    i2c_init(i2c1, 100000);
+    gpio_set_function(SI2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(SI2C_SCL, GPIO_FUNC_I2C);
+    i2c_slave_init(i2c1, 0x21, &i2cHandler);
 
-	i2c_init(i2c1, 100000);
-  gpio_set_function(SI2C_SDA, GPIO_FUNC_I2C);
-  gpio_set_function(SI2C_SCL, GPIO_FUNC_I2C);
-	i2c_slave_init(i2c1, 0x21, &i2cHandler);
+    sleep_ms(3000);
+    puts("SwingArm Sensors have started....\n");
 
+    leftSensor = std::make_shared<Sensorvl53l5cx>(vl53l5cx_i2c, PIN_LPn);
+    rightSensor = std::make_shared<Sensorvl53l5cx>(vl53l5cx_i2c, PIN_LPn2);
 
-  sleep_ms(3000);
-  puts("SwingArm Sensors have started....\n");
+    // leftSensor->setAddress(0x16);
 
-  leftSensor = std::make_shared<Sensorvl53l5cx>(vl53l5cx_i2c, PIN_LPn);
-  rightSensor = std::make_shared<Sensorvl53l5cx>(vl53l5cx_i2c, PIN_LPn2);
+    rightSensor->initialize();
 
-  //leftSensor->setAddress(0x16);
+    // if (!(leftSensor->initialize() && rightSensor->initialize())) {
+    //   return -1;
+    // }
 
-  rightSensor->initialize();
+    printf("successfully initialized!");
 
-  //if (!(leftSensor->initialize() && rightSensor->initialize())) {
-  //  return -1;
-  //}
+    multicore_launch_core1(poll);
 
-  printf("successfully initialized!");
+    while (true)
+    {
+        /*
+        printf("%d --- %d\n", leftSensor->getLatestDistance(),
+               rightSensor->getLatestDistance());
+        */
 
-  multicore_launch_core1(poll);
+        int16_t x = leftSensor->getLatestDistance();
+        int16_t y = rightSensor->getLatestDistance();
 
-  while (true) {
-    /*
-    printf("%d --- %d\n", leftSensor->getLatestDistance(),
-           rightSensor->getLatestDistance());
-    */
-    
-    int16_t x = leftSensor->getLatestDistance();
-    int16_t y = rightSensor->getLatestDistance();
-
-    slave_i2c_data.memory[0] = x >> 8;
-    slave_i2c_data.memory[1] = x & 0xFF;
-    slave_i2c_data.memory[2] = y >> 8;
-    slave_i2c_data.memory[3] = y & 0xFF;
-    sleep_ms((slave_i2c_data.memory[4] > 0) ? slave_i2c_data.memory[4] : 100);
-
-  }
+        slave_i2c_data.memory[0] = x >> 8;
+        slave_i2c_data.memory[1] = x & 0xFF;
+        slave_i2c_data.memory[2] = y >> 8;
+        slave_i2c_data.memory[3] = y & 0xFF;
+        sleep_ms((slave_i2c_data.memory[4] > 0) ? slave_i2c_data.memory[4] : 100);
+    }
 }
